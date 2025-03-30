0c8c1949ee2aada575a4d9d237aa29be8049a5bc
HocThoiJCang <buichinh157@gmail.com>
2025-03-30 23:22:21 +0700

    chatmodel

---
+++ server/models/chatModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema(
+  {
+    member: Array,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const chatModel = mongoose.model("Chat", chatSchema);
+module.exports = chatModel;
