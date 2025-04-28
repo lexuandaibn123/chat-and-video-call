2ff9e0b4bc4ec7e310e34312daa30c4a6408ccfd
jien0404 <chiendo1042004@gmail.com>
2025-04-28 13:34:13 +0700

    Merge branch 'frontend' into FE/chat-settings

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,19 @@
 const defaultRouter = require("./default");
 const authRouter = require("./auth");
 const conversationRouter = require("./conversation");
+const express = require("express");
+const apiRouter = express.Router();
 
 const route = (app) => {
-  app.use("/auth", authRouter);
-  app.use("/conversation", conversationRouter);
-  app.use("/", defaultRouter);
+  // app.use("/auth", authRouter);
+  // app.use("/conversation", conversationRouter);
+  // app.use("/", defaultRouter);
+
+  apiRouter.use("/auth", authRouter);
+  apiRouter.use("/conversation", conversationRouter);
+  apiRouter.use("/", defaultRouter);
+
+  app.use("/api", apiRouter);
 };
 
 module.exports = route;
