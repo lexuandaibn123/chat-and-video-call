9022d0a5665d4612cfb1435ad459c845268bd7db
caonamcuong <caonamcuonga1k19hht@gmail.com>
2025-04-29 15:16:14 +0700

    make friend list in homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,10 @@
 // src/pages/HomePage.jsx
 import React, { useEffect } from 'react'; // Bỏ useState nếu không dùng logout ở đây
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import FriendList from '../components/HomePage/FriendList';
 
 const HomePage = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   return (
     // Không cần div bao ngoài .home-page-container nữa vì đã có .main-content từ layout
