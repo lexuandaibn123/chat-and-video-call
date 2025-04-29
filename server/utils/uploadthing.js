19f797ded9e6e572a55dd06ed674bc3917d17ac5
lexuandaibn123 <lexuandaibn@gmail.com>
2025-04-29 10:29:05 +0700

    ci/cd for server

--- server/utils/uploadthing.js
+++ server/utils/uploadthing.js
@@ -2,7 +2,6 @@ const { createUploadthing } = require("uploadthing/express");
 const f = createUploadthing();
 
 const uploadRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
   conversationUploader: f({
     image: {
       maxFileSize: "32MB",
@@ -56,28 +55,16 @@ const uploadRouter = {
       maxFileSize: "32MB",
       maxFileCount: 1,
     },
-  })
-    // .middleware((req, res) => {
-    //   try {
-    //     const userInfo = req.session.userInfo;
-    //     console.log("userInfo: ", userInfo);
-    //     if (!userInfo) return res.status(401).json({ error: "Unauthorized" });
-    //     return { userInfo };
-    //   } catch (error) {
-    //     console.error(error);
-    //     return res.status(500).json({ error: "Internal server error" });
-    //   }
-    // })
-    .onUploadComplete(({ file }) => {
-      return {
-        success: true,
-        url: file.ufsUrl,
-        fileHash: file.fileHash,
-        name: file.name,
-        size: file.size,
-        type: file.type,
-      };
-    }),
+  }).onUploadComplete(({ file }) => {
+    return {
+      success: true,
+      url: file.ufsUrl,
+      fileHash: file.fileHash,
+      name: file.name,
+      size: file.size,
+      type: file.type,
+    };
+  }),
 };
 
 module.exports = { uploadRouter };
