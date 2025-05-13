ea80fc366160877300d5b922d7b7a61bfdee9011
lexuandaibn123 <lexuandaibn@gmail.com>
2025-05-13 10:22:58 +0700

    debug video call

--- client/src/components/VideoCall/SFUClient.js
+++ client/src/components/VideoCall/SFUClient.js
@@ -12,6 +12,11 @@ export default function SFUClient(url, userId, onStreamAdded, onStreamRemoved) {
     iceServers: [
       { urls: "stun:stun.stunprotocol.org:3478" },
       { urls: "stun:stun.l.google.com:19302" },
+      {
+        urls: "turn:54.251.71.205:3478",
+        username: "seedlabs",
+        credential: "seedlabsturn",
+      },
     ],
   };
   this.onStreamAdded = onStreamAdded;
